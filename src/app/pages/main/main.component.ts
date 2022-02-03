7e38c79508a4e9784bc74331178b9ec6a1c1c395
ianmajkut <ianedwinmajkut@gmail.com>
2022-02-03 13:46:53 -0300

    feat: hide buttons dynamically, add number blocks field, show resting and working countdown, change the way timers works
    To use .unsubscribe clicking the pause button, i had to create 2 Subscriptions. The methods restTimer() and workingTimer() handle the intervals and everything related to the countdown and assign it to previosly created subscriptions created then return it

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ThemePalette } from '@angular/material/core';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
-import { TimerService } from 'src/app/services/timer.service';
-import { interval, timer } from 'rxjs';
+import { interval, Observable, Subscription, timer } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 interface Task {
@@ -30,40 +29,119 @@ export class MainComponent {
   timerInterval = interval(1000);
   currentWorkingTime: number = 0;
   currentRestingTime: number = 0;
+  showingTime!: number;
+  workingTimeForSpinner!: number;
+  restingTimeForSpinner!: number;
+  showPlayButton: boolean = true;
+  showPauseButton: boolean = false;
+  showStopButton: boolean = false;
+  roundsBlocks!: number;
+  workingSubs!: Subscription;
+  restingSubs!: Subscription;
+
+  typeOfBlock!: string;
 
   myForm: FormGroup = this.formBuilder.group({
     taskName: [, [Validators.required]],
     workingTime: [, [Validators.required, Validators.min(1)]],
+    amountBlocks: [, [Validators.required, Validators.min(1)]],
     restTime: [, [Validators.required, Validators.min(1)]],
     description: [,]
   });
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private timerService: TimerService
-  ) {}
+  constructor(private formBuilder: FormBuilder) {}
 
+  //TODO make changes because, clicking the 'play' button, it
+  //starts again and not resume the timer running
   saveTask() {
-    const { workingTime, restTime } = this.myForm.value;
+    const { workingTime, amountBlocks } = this.myForm.value;
+    this.workingTimeForSpinner = workingTime;
+    this.roundsBlocks = amountBlocks;
     this.tasks.push(this.myForm.value);
-    this.myForm.reset();
+    this.showPauseButton = true;
+    this.showStopButton = true;
     //console.log(this.myForm.value);
     // console.log(this.tasks);
-    // this.timerService.workingTimer(workingTime);
-    this.workingTimer(workingTime);
-    //this.timerService.restTimer(restTime);
+    this.workingTimer(this.workingTimeForSpinner * 60, this.roundsBlocks);
+    //await this.restTimer(restTime);
   }
 
-  workingTimer(workTime: number) {
-    const timeWork = workTime;
-    const countDownWork = this.timerInterval.pipe(take(timeWork));
-    return countDownWork.subscribe((val) => {
-      this.currentWorkingTime = timeWork - (val + 1);
-      this.value = (this.currentWorkingTime * 100) / timeWork;
-      console.log(`Work timing ${this.currentWorkingTime}`);
-      if (this.currentWorkingTime == 0) {
-        console.log('Time is UP');
-      }
-    });
+  pauseTask() {
+    this.workingSubs.unsubscribe();
+  }
+
+  workingTimer(workTime: number, rounds: number) {
+    if (rounds > 0) {
+      this.typeOfBlock = 'Working Time Countdown:';
+      --rounds;
+      this.color = 'warn';
+      const timeWork = workTime;
+      /*
+      const countDownWork = this.timerInterval.pipe(take(timeWork));
+      return countDownWork.subscribe((val) => {
+        this.currentWorkingTime = timeWork - (val + 1);
+        this.value = (this.currentWorkingTime * 100) / timeWork;
+        console.log(`Work timing ${this.currentWorkingTime} sec`);
+        if (this.currentWorkingTime == 0) {
+          const { restTime } = this.myForm.value;
+          this.restingTimeForSpinner = restTime;
+          this.restTimer(this.restingTimeForSpinner * 60, rounds);
+        }
+      });
+      */
+      this.workingSubs = this.timerInterval
+        .pipe(take(timeWork))
+        .subscribe((val) => {
+          this.currentWorkingTime = timeWork - (val + 1);
+          this.showingTime = this.currentWorkingTime;
+          //Progress Spinner value
+          this.value = (this.currentWorkingTime * 100) / timeWork;
+          //console.log(`Work timing ${this.currentWorkingTime} sec`);
+          if (this.currentWorkingTime == 0) {
+            const { restTime } = this.myForm.value;
+            this.restingTimeForSpinner = restTime;
+            this.restTimer(this.restingTimeForSpinner * 60, rounds);
+          }
+        });
+      return this.workingSubs;
+    }
+    this.typeOfBlock = '';
+    this.showingTime = 0;
+    return;
+  }
+
+  restTimer(restTime: number, rounds: number) {
+    if (rounds > 0) {
+      this.typeOfBlock = 'Resting Time Countdown:';
+      this.color = 'primary';
+      const timeRest = restTime;
+      /*
+      const countDownRest = this.timerInterval.pipe(take(timeRest));
+      return countDownRest.subscribe((val) => {
+        this.currentRestingTime = timeRest - (val + 1);
+        this.value = (this.currentRestingTime * 100) / restTime;
+        console.log(`Rest timing ${this.currentRestingTime} sec`);
+        if (this.currentRestingTime == 0) {
+          this.workingTimer(this.workingTimeForSpinner * 60, rounds);
+        }
+      });
+      */
+      this.restingSubs = this.timerInterval
+        .pipe(take(timeRest))
+        .subscribe((val) => {
+          this.currentRestingTime = timeRest - (val - 1);
+          this.showingTime = this.currentRestingTime;
+          //Progress Spinner value
+          this.value = (this.currentRestingTime * 100) / timeRest;
+          //console.log(`Rest timing ${this.currentRestingTime} sec`);
+          if (this.currentRestingTime == 0) {
+            this.workingTimer(this.workingTimeForSpinner * 60, rounds);
+          }
+        });
+      return this.restingSubs;
+    }
+    this.typeOfBlock = '';
+    this.showingTime = 0;
+    return;
   }
 }
